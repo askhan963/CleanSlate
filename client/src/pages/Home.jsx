653a3d2a3cc63a8a69951c0f7a0c5f140562cfbe
askhan963 <awaisknas963@gmail.com>
2024-11-01 19:43:48 +0500

    Testimonials Component

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import BgSlider from "../components/BgSlider";
 import Header from "../components/Header";
 import Steps from "../components/Steps";
+import Testimonials from "../components/Testimonials";
 
 const Home = () => {
   return (
@@ -8,6 +9,7 @@ const Home = () => {
       <Header />
       <Steps />
       <BgSlider />
+      <Testimonials />
     </div>
   );
 };

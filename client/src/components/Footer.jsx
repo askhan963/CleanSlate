c862f86d3f8c76ca4707912156749248073825b4
askhan963 <awaisknas963@gmail.com>
2024-11-01 16:16:09 +0500

    Nav & Footer Components

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import { assets } from "../assets/assets";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className="flex items-center justify-between gap-4 px-4 lg:px-44 py-3">
+      <div className="flex items-center gap-2">
+        <img
+          className="w-12 h-12"
+          src={assets.logo_icon}
+          alt="CleanSlate Logo"
+        />
+        <span className="text-lg font-semibold">CleanSlate</span>
+      </div>
+      <p className="flex-1 border-l border-gray-400 pl-4 text-sm text-gray-500 hidden sm:block">
+        Copyright © Muhammad Awais Khan | All rights reserved.
+      </p>
+      <div className="flex gap-2">
+        <img
+          className="w-8 h-8"
+          src={assets.facebook_icon}
+          alt="Facebook Icon"
+        />
+        <img className="w-8 h-8" src={assets.twitter_icon} alt="Twitter Icon" />
+        <img
+          className="w-8 h-8"
+          src={assets.google_plus_icon}
+          alt="Google Plus Icon"
+        />
+      </div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import { assets } from "../assets/assets";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div className="flex items-center justify-between mx-4 py-3 lg:mx-44">
+      <Link to="/" className="flex items-center gap-2">
+        <img
+          className="w-12 h-12 sm:w-14 sm:h-14"
+          src={assets.logo_icon}
+          alt="CleanSlate Logo"
+        />
+        <span className="text-lg font-semibold">CleanSlate</span>
+      </Link>
+      <button className="bg-zinc-800 text-white flex items-center gap-4 px-4 py-2 sm:px-8 sm:py-3 text-sm rounded-full">
+        Get Started{" "}
+        <img className="w-3 sm:w-4" src={assets.arrow_icon} alt="Arrow Icon" />
+      </button>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;

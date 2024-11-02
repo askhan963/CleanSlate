c7c7246b8f366c2967f2d8ecec3fdcf972b8c3d0
askhan963 <awaisknas963@gmail.com>
2024-11-02 19:37:37 +0500

    db change

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose'
 const connectDB = async () => {
     try {
         console.log(process.env.MONGO_URI)
-        const conn = await mongoose.connect(process.env.MONGO_URI+"/CleanSlate", {
+        const conn = await mongoose.connect(process.env.MONGO_URI, {
           useNewUrlParser: true,
           useUnifiedTopology: true,
         });

144fca583a59e52f9ae296caddcb078b88ad70b7
askhan963 <awaisknas963@gmail.com>
2024-11-04 13:54:38 +0500

    bg-removal API

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 import express from "express";
 import userRoutes from './userRoutes.js'
+import imageRoutes from "./imageRoutes.js";
 const router = express.Router();
 
 router.use("/user", userRoutes)
+router.use("/image", imageRoutes)
 
 export default router;
